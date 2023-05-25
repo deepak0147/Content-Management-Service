a6168af038b26a5b621ab69a06d08dadfd5f6e83
deepak0147 <deepaksri.ch@gmail.com>
2023-05-26 00:19:59 +0530

    intial commit

---
+++ src/main/java/com/dpk/cms/ContentManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dpk.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentManagementServiceApplication.class, args);
+	}
+
+}
